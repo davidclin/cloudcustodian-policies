be7bdd36da7f07e6aaaeec9507c92af7ca8c21a2
David Lin <davidclin@users.noreply.github.com>
2019-01-09 13:30:52 -0800

    Update cleancache.sh
    minor update

--- cleancache.sh
+++ cleancache.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# to find path of cache issue `sudo find / cloud-custodian.cache
+
+# for ubuntu
 rm ~/.cache/cloud-custodian.cache
 
+# for centos
+# rm /home/ec2-user/.cache/cloud-custodian.cache
+
+echo 'Cloud Custodian cache deleted'
